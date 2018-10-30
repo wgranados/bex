c4aaaae9c4a9881c1be3c3a9e417d47f734159cf
Maged Hennawy <maged.hennawy@ibm.com>
2018-10-29 23:45:48 -0400

    Settings page

--- src/containers/Settings/index.js
+++ src/containers/Settings/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Select from 'react-select';
+import { Button, DropdownV2, TextInput} from 'carbon-components-react';
+import './styles.css'
 
 class Settings extends Component  {
     state = {
@@ -21,71 +23,96 @@ class Settings extends Component  {
             selectedFontSize,
             selectedBackgroundColour,
         } = this.state;
-        const languageOptions = [
-            { value: "english", label: "English - EN"},
-            { value: "spanish", label: "Spanish - ES"},
-            { value: "french", label: "French - FE"},
-        ];
-        const fontOptions = [
-            { value: "Times New Roman", label: "Times New Roman"},
-            { value: "Arial", label: "Arial"},
-            { value: "Arial Black", label: "Arial Black"},
-            { value: "Comic Sans", label: "Comic Sans"},
-            { value: "Wingdings", label: "Wingdings"},
-        ];
-        const fontSizeOptions = [
-            { value: "12", label: "12pt"},
-            { value: "13", label: "13pt"},
-            { value: "14", label: "14pt"},
-            { value: "15", label: "15pt"},
-        ];
-        const backgroundColorOptions = [
-            { value: "blue", label: "Blue"},
-            { value: "white", label: "White"},
-        ];
         return (
             <div>
                 <center>
                     <h1>Settings</h1>
                     <div id="accessibility">
-                    <p>Language</p>
-                    <div style={{width: 200}}>
-                        <Select
-                            value={selectedLanguage}
+                        <p>Language</p>
+                        <div style={{width: 300}}>
+                            <DropdownV2
+                            label="Language"
+                            ariaLabel="Dropdown"
+                            items={[
+                                { id: "english", text: "English - EN"},
+                                { id: "spanish", text: "Spanish - ES"},
+                                { id: "french", text: "French - FE"},
+                            ]}
+                            itemToString={item => (item ? item.text : '')}
                             onChange={this.handleLanguageChange}
-                            options={languageOptions} />
-                    </div>
-                    <p>Font</p>
-                    <div style={{width: 200}}>
-                        <Select
-                            value={selectedFont}
+                            />
+                        </div>
+
+                        <p>Font</p>
+                        <div style={{width: 300}}>
+                            <DropdownV2
+                            label="Font"
+                            ariaLabel="Dropdown"
+                            items={[
+                                { id: "Times New Roman", text: "Times New Roman"},
+                                { id: "Arial", text: "Arial"},
+                                { id: "Arial Black", text: "Arial Black"},
+                                { id: "Comic Sans", text: "Comic Sans"},
+                                { id: "Wingdings", text: "Wingdings"},
+                            ]}
+                            itemToString={item => (item ? item.text : '')}
                             onChange={this.handleFontChange}
-                            options={fontOptions} />
-                    </div>
-                    <p>Font Size</p>
-                    <div style={{width: 200}}>
-                        <Select
-                            value={selectedFontSize}
+                            />
+                        </div>
+
+                        <p>Font Size</p>
+                        <div style={{width: 300}}>
+                            <DropdownV2
+                            label="Font Size"
+                            ariaLabel="Dropdown"
+                            items={[
+                                { id: "12", text: "12pt"},
+                                { id: "13", text: "13pt"},
+                                { id: "14", text: "14pt"},
+                                { id: "15", text: "15pt"},
+                            ]}
+                            itemToString={item => (item ? item.text : '')}
                             onChange={this.handleFontSizeChange}
-                            options={fontSizeOptions} />
-                    </div>
-                    <p>Background Colour</p>
-                    <div style={{width: 200}}>
-                        <Select
-                            value={selectedBackgroundColour}
+                            />
+                        </div>
+
+                        <p>Background Colour</p>
+                        <div style={{width: 300}}>
+                            <DropdownV2
+                            label="Background Colour"
+                            ariaLabel="Dropdown"
+                            items={[
+                                { id: "blue", text: "Blue"},
+                                { id: "white", text: "White"},
+                            ]}
+                            itemToString={item => (item ? item.text : '')}
                             onChange={this.handleBackgroundColourChange}
-                            options={backgroundColorOptions} />
+                            />
+                        </div>
                     </div>
 
-
-
-                    </div>
-                    <div id="login">
                         <h3>Login</h3>
-                        <p>Email: <input></input></p>
-                        <p>Password: <input></input> </p>
-                        <button>reset password</button>
-                        <button>link facebook</button>
+                        <div id="login">
+                        <TextInput
+                            className="text-input"
+                            id="test2"
+                            labelText="Email"
+                            type=""
+                            placeholder="Email"
+                            hideLabel={true}
+                            invalidText="A valid value is required"
+                        />
+                        <TextInput
+                            className="text-input"
+                            id="test2"
+                            labelText="Password"
+                            type=""
+                            placeholder="Password"
+                            hideLabel={true}
+                            invalidText="A valid value is required"
+                        />
+                        <Button className="test">reset password</Button>
+                        <Button className="test">link facebook</Button>
                     </div>
                 </center>
 
