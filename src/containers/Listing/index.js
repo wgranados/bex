4df8f0d9e5d797f132120656e383c7e4f4f05543
William Granados <william.granadosmatamoros@mail.utoronto.ca>
2018-11-22 18:41:50 -0500

    updated listing allignment with information to be on the side

--- src/containers/Listing/index.js
+++ src/containers/Listing/index.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import ImageGallery from 'react-image-gallery'
+import './styles.css';
 
 const tardosImage = require('./tardos.jpg');
+
 
 class Listing extends Component {
     render() {
@@ -14,29 +17,32 @@ class Listing extends Component {
             location,
         } = this.props.location.state;
         const { className } = this.props;
-        console.log(className);
 
         return (
-            <div id="listing">
+            <div id="listing" className="wrapper">
                 <center>
                     <h1>{title}</h1>
-                    <img src={tardosImage} height="400px" width="400px"/>
-                    <p className={className} ><b>Price:</b> {price}</p>
-                    <p className={className} ><b>Title:</b> {title}</p>
-                    <p className={className} ><b>Author:</b> {author}</p>
-                    <p className={className} ><b>Edition:</b> {edition}</p>
-                    <p className={className} ><b>ISBN Number:</b> {isbnNumber}</p>
-                    <p className={className} ><b>Condition:</b> Good</p>
-                    <p className={className} ><b>Location:</b> {location}</p>
-                    <p className={className} ><b>Seller:</b> <a href="">wgma</a> </p>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <td> <button>Contact Seller by Email</button></td>
-                                <td> <button>Contact Seller by Messenger</button></td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <div id="image" className="leftcolumn">
+                        <img src={tardosImage} height="400px" width="400px"/>
+                    </div>
+                    <div id="information" className="rightcolumn">
+                        <p align="left" className={className} ><b>Price:</b> {price}</p>
+                        <p align="left" className={className} ><b>Title:</b> {title}</p>
+                        <p align="left" className={className} ><b>Author:</b> {author}</p>
+                        <p align="left" className={className} ><b>Edition:</b> {edition}</p>
+                        <p align="left" className={className} ><b>ISBN Number:</b> {isbnNumber}</p>
+                        <p align="left" className={className} ><b>Condition:</b> Good</p>
+                        <p align="left" className={className} ><b>Location:</b> {location}</p>
+                        <p align="left" className={className} ><b>Seller:</b> <a href="">wgma</a> </p>
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td> <button>Contact Seller by Email</button></td>
+                                    <td> <button>Contact Seller by Messenger</button></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </center>
 
             </div>
