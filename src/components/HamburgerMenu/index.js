d0aa90bf47f91b77a456e2cfab51ca137ecb0e3d
William Granados <william.granadosmatamoros@mail.utoronto.ca>
2018-11-22 19:40:31 -0500

    updated sidenav to include shortcuts more that are easier to navigate to

--- src/components/HamburgerMenu/index.js
+++ src/components/HamburgerMenu/index.js
@@ -1,23 +1,65 @@
 import React, { Component } from 'react';
 
 import { slide as Menu } from 'react-burger-menu'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Redirect, Route, Link } from 'react-router-dom';
 
 import './styles.css'
 
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+
+
 
 class HamburgerMenu extends React.Component {
-  showSettings (event) {
-    event.preventDefault();
+  state = {
+    value: '',
+    redirect: false,
   }
 
+  onSelect = (selected) => {
+    this.setState({...this.state, value: selected, redirect: true});
+  }
+
+
   render () {
+    if(this.state.redirect) {
+          this.setState({...this.state, redirect: false});
+          return <Redirect to={{
+              pathname: process.env.PUBLIC_URL + "/" + this.state.value,
+              state: {search: this.state.value} }} />
+    }
       return (
-        <Menu>
-            <Link to={process.env.PUBLIC_URL + `/`}>Home</Link>
-            <Link to={process.env.PUBLIC_URL + `/createlisting`}>Create Listing</Link>
-            <Link to={process.env.PUBLIC_URL + `/settings`}>Settings</Link>
-        </Menu>
+        <SideNav
+    onSelect={this.onSelect}
+>
+    <SideNav.Toggle />
+    <SideNav.Nav defaultSelected="home">
+        <NavItem eventKey="">
+            <NavIcon>
+                <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+                Home
+            </NavText>
+        </NavItem>
+        <NavItem eventKey="createlisting">
+            <NavIcon>
+                <i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+              Create Listing 
+            </NavText>
+        </NavItem>
+        <NavItem eventKey="settings">
+            <NavIcon>
+                <i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+              Settings
+            </NavText>
+        </NavItem>
+    </SideNav.Nav>
+</SideNav>
       );
   }
 }
